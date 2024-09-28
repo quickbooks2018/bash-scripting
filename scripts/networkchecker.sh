392ec38ba38909f2759cc8669541efefa942b1af
Muhammad Asim <asim@Muhammads-MacBook-Pro.local>
2024-09-28 10:39:50 +0500

    checking internet connectivity

--- scripts/networkchecker.sh
+++ scripts/networkchecker.sh
@@ -15,24 +15,41 @@
 # 1 specifies the number of seconds to wait
 # -v is the verbose option
 
-domain='google.com'
-port=443
+# Check if the domain is provided
+check_domain() {
+    if [ $# -eq 0 ]; then
+        echo "Error: This script $(basename "$0") requires a domain name as an argument"
+        exit 1
+    fi
+}
 
 # Function to check internet connectivity
 check_internet() {
+    local domain="$1"
+    local port=443
     # Capture the current date and time
     local current_datetime=$(date +"%Y-%m-%d %H:%M:%S")
 
     if nc -zw3 "$domain" "$port" 2>/dev/null; then
         echo "$current_datetime - Internet is working"
+
+        # When return 0 is used, it means the function executed successfully without any errors.
         return 0
     else
         echo "$current_datetime - Internet is not working"
+
+        # When return 1 is used, it means the function encountered an error.
         return 1
     fi
 }
 
-# Perform the check
-check_internet
+# Main execution
+# $@: This is an array of all the arguments provided to the script
+check_domain "$@"
+
+# $1: This is the first argument provided to the script
+check_internet "$1"
+
+exit $?
 
 #End
--- scripts/positional_param.sh
+++ scripts/positional_param.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# $0: This is the name of the script
 echo Name of the scrit is $0
 echo Name of the script is `basename $0`
+
+# $1: This is the first argument provided to the script
 echo First argument is $1
+
+# $@: This is an array of all the arguments provided to the script
+echo All arguments are $@
+
+# $#: This is the number of arguments provided to the script
+echo Number of arguments are $#
